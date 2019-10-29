ab7dc95810bf2c942a984a6e8b2ee8500f794aaa
Jonathan Choi <jonathan.choi90@gmail.com>
2019-10-29 17:11:23 -0400

    switched to Java class, linked Login and CreateAcc Buttons to methods

---
+++ Bite4Byte/app/src/main/java/com/example/bite4byte/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.bite4byte;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onLoginClick(View view) {
+
+    }
+
+    public void onCreateAccClick(View view) {
+
+    }
+
+}
