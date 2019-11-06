04d4f8900296317414ed27a64b10a4319d5e54af
kuchaudhari <chaudhari.kunaal@gmail.com>
2019-11-06 01:18:30 -0500

    Local data storage architecture

---
+++ Bite4Byte/app/src/main/java/com/example/bite4byte/InternalData/Data.java
@@ -0,0 +1,73 @@
+package com.example.bite4byte.InternalData;
+
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+
+import static android.content.Context.MODE_PRIVATE;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+
+public class Data {
+    /*
+    * JSONObject Fields
+    * "username"
+    * "firstname"
+    * "lastname"
+    * "password"
+    * "preferences"
+    * "allergies"
+    */
+    Map<String, JSONObject> accountMap;
+    String accountFileName = "Accounts.json";
+
+    public Data() throws IOException, ParseException {
+        JSONParser parser = new JSONParser();
+        JSONArray viols = (JSONArray)parser.parse(new FileReader(accountFileName));
+
+        Iterator <JSONObject> iter = viols.iterator();
+        accountMap = new HashMap<String, JSONObject>();
+        while (iter.hasNext()) {
+            JSONObject a = (JSONObject) iter.next();
+            try {
+                accountMap.put((String) a.get("username"), a);
+            } catch (Exception e) {
+                continue;
+            }
+        }
+    }
+
+    //returns true if account is successfully created and false if username is already in use
+    public boolean createAccount(String username, String firstname, String lastname, String password, String[] preferences, String[] allergies) {
+        if (accountMap.containsKey(username)) {
+            return false;
+        }
+        JSONObject newAccount = new JSONObject();
+        newAccount.put("username", username);
+        newAccount.put("firstname", firstname);
+        newAccount.put("lastname", lastname);
+        newAccount.put("password", password);
+        newAccount.put("preferences", preferences);
+        newAccount.put("allergies", allergies);
+        return true;
+    }
+
+    //returns the JSONObject for the username and null if username, pswd is invalid
+    public JSONObject login(String usernameIn, String passwordIn) {
+        if (!accountMap.containsKey(usernameIn)) {
+            return null;
+        }
+        if (accountMap.get(usernameIn).get("password") == passwordIn) {
+            return accountMap.get(usernameIn);
+        }
+        return null;
+    }
+
+
+}
